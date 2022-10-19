558271a16c0b38b895dffb1371d4f9ac4aaaf546
jh <jh@dwq.com>
2022-10-19 15:25:54 +0800

    first commit

---
+++ src/main/java/com/ecjtu/GitTest.java
@@ -0,0 +1,11 @@
+package com.ecjtu;
+
+/**
+ * @author lenovo
+ * @create 2022-10-19 15:22
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
